2a4f60e0d94c0a0813dcc97f53907f69d78871f7
Romtoc <objcube@mail.com>
2015-04-12 18:21:16 +0530

    Client Not Disconnecting - Fix

--- Drivers/Socket.pm
+++ Drivers/Socket.pm
@@ -29,7 +29,10 @@ method serverLoop {
           eval {
              my $objClient = $self->getClientBySock($resSock);              
              my $strBuffer;
-             $resSock->sysread($strBuffer, 65536);
+             my $intBytes = $resSock->sysread($strBuffer, 65536);
+             if ($intBytes <= 0 || $strBuffer eq '') {
+                 return $self->removeClient($objClient->{sock});
+             }
              my @arrData = split(chr(0), $strBuffer);
              foreach (@arrData) {                         
 				                 $self->handleData($_, $objClient);    
