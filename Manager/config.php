1b6be9ad900f73d3cf702851eb086f4cfe63df18
Levi-M <objcube@mail.com>
2016-01-25 01:17:43 +0530

    Use 127.0.0.1 instead of localhost

--- Manager/config.php
+++ Manager/config.php
@@ -1,12 +1,12 @@
 <?php
 
-$strDBHost = 'localhost';
+$strDBHost = '127.0.0.1';
 $strDBUser = 'root';
 $strDBPass = 'passwordhere';
 $strDBName = 'Luna';
 
 // for server statistics
-$strServerHost = 'localhost';
+$strServerHost = '127.0.0.1';
 $intLoginPort = 6112;
 $intGamePort = 6113;
 
