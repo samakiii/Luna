51c65679d0fe96dd158ffb48ab0e9436dc0da76f
Levi-M <objcube@mail.com>
2016-02-04 00:39:03 +0530

    Huge security update for Paypal

--- Website/manager/paypal/success.php
+++ Website/manager/paypal/success.php
@@ -10,29 +10,74 @@ $username = $_SESSION['login_user'];
 
 $item_no = $_GET['item_number'];
 $item_transaction = $_GET['tx']; 
-$item_status = $_GET['st'];
 
-$item_no = mysqli_real_escape_string($mysql, $item_no);
-$item_transaction = mysqli_real_escape_string($mysql, $item_transaction);
-$item_status = mysqli_real_escape_string($mysql, $item_status);
+if (isset($item_transaction)) { 
+    $item_no = mysqli_real_escape_string($mysql, $item_no);
+    $item_transaction = mysqli_real_escape_string($mysql, $item_transaction);
+    
+    $item_no = addslashes($item_no);
+    $item_transaction = addslashes($item_transaction);
+    
+/*
+    Follow these steps to configure your account for PDT:
+    
+       1. Log in to your PayPal account.
+       2. Click the Profile subtab.
+       3. Click Website Payment Preferences in the Seller Preferences column.
+       4. Under Auto Return for Website Payments, click the On radio button.
+       5. For the Return URL, enter the URL on your site that will receive the transaction ID posted by PayPal after a customer payment.
+       6. Under Payment Data Transfer, click the On radio button.
+       7. Click Save.
+       8. Click Website Payment Preferences in the Seller Preferences column.
+       9. Scroll down to the Payment Data Transfer section of the page to view your PDT identity token.
+       10. Copy and paste that in $identity_token
+*/
+    
+    $identity token = '';
+    
+    $request = curl_init();
+    curl_setopt_array($request, 
+                               array(
+                                      CURLOPT_URL => 'https://www.paypal.com/cgi-bin/webscr', 
+                                      CURLOPT_POST => TRUE, 
+                                      CURLOPT_POSTFIELDS => http_build_query(
+                                      array(
+                                            'cmd' => '_notify-synch', 
+                                            'tx' => $item_transaction, 
+                                            'at' => $identity_token
+                                      )),
+                                      CURLOPT_RETURNTRANSFER => TRUE,
+                                      CURLOPT_HEADER => FALSE
+                               )
+    );
+    $payment_response = curl_exec($request);
+    curl_close($request);
+    
+    $payment_response = substr($payment_response, 7);
+    $payment_response = urldecode($payment_response);
+    preg_match_all('/^([^=\s]++)=(.*+)/m', $payment_response, $message, PREG_PATTERN_ORDER);
+    $payment_response = array_combine($message[1], $message[2]);
+    if (isset($payment_response['charset']) && strtoupper($payment_response['charset']) !== 'UTF-8') {
+       foreach($payment_response as $key => $value) {
+                   $value = mb_convert_encoding($value, 'UTF-8', $payment_response['charset']);
+       }
+       $payment_response['charset_original'] = $payment_response['charset'];
+       $payment_response['charset'] = 'UTF-8';
+   }
+   ksort($payment_response);
 
-$item_no = addslashes($item_no);
-$item_transaction = addslashes($item_transaction);
-$item_status = addslashes($item_status);
-
-if (!empty($item_transaction) && $item_status == "Completed") {
-    $result = mysqli_query($mysql, "INSERT INTO sales (`pid`, `uid`, `saledate`, `transactionid`) VALUES ('" . $item_no . "', '" . $uid . "', NOW(), '" . $item_transaction . "')");
-    if ($result) {
-        $intPaymentID = mysqli_insert_id($mysql);
-        mysqli_query($mysql, "UPDATE users SET isVIP = '1' WHERE username = '$username'");
-        mysqli_query($mysql, "UPDATE users SET rank = '2' WHERE username = '$username'");
-        echo "<center><h2>Welcome, $username</h1></center>";
-        echo "<center><h2>Payment Successful</h1></center>";
-        echo "<center><p>Your Payment ID - " . $intPaymentID . "</p></center>";
-        echo "<center><p>Your rank has been successfully upgraded to VIP</p></center>";
-    }
-} else {
-    echo "<center><h2>Payment Failed</h2></center>";
+   if (strtoupper($payment_response['payment_status']) == 'COMPLETED') {
+       $result = mysqli_query($mysql, "INSERT INTO sales (`pid`, `uid`, `saledate`, `transactionid`) VALUES ('" . $item_no . "', '" . $uid . "', NOW(), '" . $item_transaction . "')");
+       $intPaymentID = mysqli_insert_id($mysql);
+       mysqli_query($mysql, "UPDATE users SET isVIP = '1' WHERE username = '$username'");
+       mysqli_query($mysql, "UPDATE users SET rank = '2' WHERE username = '$username'");
+       echo "<center><h2>Welcome, $username</h1></center>";
+       echo "<center><h2>Payment Successful</h1></center>";
+       echo "<center><p>Your Payment ID - " . $intPaymentID . "</p></center>";
+       echo "<center><p>Your rank has been successfully upgraded to VIP</p></center>";
+   } else {
+       echo "<center><h2>Payment Failed</h2></center>";
+   }
 }
 
 ?>
