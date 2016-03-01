d85e67ba65a56b79c4b8d5cac937e8f3f9285ad5
Lynx <Levi-M@users.noreply.github.com>
2016-03-01 23:35:33 +0530

    Update commands.php

--- Website/commands.php
+++ Website/commands.php
@@ -1,165 +1,278 @@
-<?php
-
-include 'session.php';
-include '../config.php';
-
-if ($_SESSION['isVIP'] == false) { 
-    header('location: index.php');
-}
-
-$strError = '';
-$strMessage = '';
-
-$mysql = mysqli_connect($strDBHost, $strDBUser, $strDBPass, $strDBName);
-
-if (isset($_POST['update'])) {
-    $strNG = $_POST['nameglow'];
-    $strNC = $_POST['namecolor'];
-    $strBT = $_POST['bubbletext'];
-    $strBC = $_POST['bubblecolor'];
-    $strRC = $_POST['ringcolor'];
-    $strCG = $_POST['chatglow'];
-    $strPG = $_POST['penguinglow'];
-    $intSpeed = $_POST['speed'];
-
-    if (isset($strNG) && isset($strNC) && isset($strBT) && isset($strBC) && isset($strRC) && isset($strCG) && isset($intSpeed) && isset($strPG)) {
-        $strNG = mysqli_real_escape_string($mysql, $strNG);
-        $strNC = mysqli_real_escape_string($mysql, $strNC);
-        $strBT = mysqli_real_escape_string($mysql, $strBT);
-        $strBC = mysqli_real_escape_string($mysql, $strBC);
-        $strRC = mysqli_real_escape_string($mysql, $strRC);
-        $strCG = mysqli_real_escape_string($mysql, $strCG);
-        $strPG = mysqli_real_escape_string($mysql, $strPG);
-        $intSpeed = mysqli_real_escape_string($mysql, $intSpeed);
-        
-        $strNG = addslashes($strNG);
-        $strNC = addslashes($strNC);
-        $strBT = addslashes($strBT);
-        $strBC = addslashes($strBC);
-        $strRC = addslashes($strRC);
-        $strCG = addslashes($strCG);
-        $strPG = addslashes($strPG);
-        $intSpeed = addslashes($intSpeed);
-
-        $strUsername = $_SESSION['login_user'];
-        
-        if (is_numeric($intSpeed) && $intSpeed <= 100) {
-            if (ctype_alnum($strNG) && strlen($strNG) <= 6) {
-                if (ctype_alnum($strNC) && strlen($strNC) <= 6) {
-                    if (ctype_alnum($strBT) && strlen($strBT) <= 6) {
-                        if (ctype_alnum($strBC) && strlen($strBC) <= 6) {
-                            if (ctype_alnum($strRC) && strlen($strRC) <= 6) {
-                                if (ctype_alnum($strCG) && strlen($strCG) <= 6) {
-									if (ctype_alnum($strPG) && strlen($strPG) <= 6) {
-										$strNGHex = '0x' . $strNG;
-										$strNCHex = '0x' . $strNC;
-										$strBTHex = '0x' . $strBT;
-										$strBCHex = '0x' . $strBC;
-										$strRCHex = '0x' . $strRC;
-										$strCGHex = '0x' . $strCG;
-										$strPGHex = '0x' . $strPG;
-										mysqli_query($mysql, "UPDATE users SET nameglow = '$strNGHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET namecolour = '$strNCHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET bubbletext = '$strBTHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET bubblecolour = '$strBCHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET ringcolour = '$strRCHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET chatglow = '$strCGHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET penguinglow = '$strPGHex' WHERE username = '$strUsername'");
-										mysqli_query($mysql, "UPDATE users SET speed = '$intSpeed' WHERE username = '$strUsername'");
-										$strMessage = 'Successfully updated glow settings';
-								   } else {
-									  $strError = 'Invalid Penguin Glow Pattern';
-								   }
-                                } else {
-                                    $strError = 'Invalid Chat Glow Pattern';
-                                }
-                            } else {
-                                $strError = 'Invalid Ring Color Pattern';
-                            }
-                        } else {
-                            $strError = 'Invalid Bubble Color Pattern';
-                        }
-                    } else {
-                        $strError = 'Invalid Bubble Text Pattern';
-                    }
-                } else {
-                    $strError = 'Invalid Name Color Pattern';
-                }
-            } else {
-                $strError = 'Invalid Name Glow Pattern';
-            }
-        } else {
-            $strError = 'Invalid Penguin Speed';
-        }
-    }
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Luna - Manager</title>
-<link rel="stylesheet" href="../css/style.css">
-<link rel='stylesheet prefetch' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
+<title>Luna - Commands</title>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+	
 <div class="overlay">
 <ul>
-<li><a href="profile.php">Home</a></li>
-<li><a href="settings.php">Settings</a></li>
-<li><a href="search.php">Search</a></li>
-<?php if ($_SESSION['isVIP'] == true) { ?>
-<li><a class="active" href="glows.php">Glow Panel</a></li>
-<?php 
-}
-if ($_SESSION['isStaff'] == true) {
- ?>
- <li><a href="moderator.php">Mod Panel</a></li>
-<?php if ($_SESSION['isAdmin'] == true) { ?>
-<li><a href="admin.php">Admin Panel</a></li>
-<?php } } ?>
-<li><a href="store.php">Store</a></li>
-<li><a href="server.php">Server</a></li>
-<li><a href="logout.php">Logout</a></li>
+<li><a href="index.php">Home</a></li>
+<li><a href="register.php">Register</a></li>
+<li><a href="play.php">Play</a></li>
+<li><a href="manager/index.php">Manager</a></li>
+<li><a class="active" href="commands.php">Commands</a></li>
+<li><a href="staff.php">Staff</a></li>
+<li><a href="contact.php">Contact Us</a></li>
+<li><a href="about.php">About Us</a></li>
 </ul>     
 
 <div class="container">
-<center>
-<form class="form" name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-       <label for="nameglow">Name Glow:</label>
-       <input class="jscolor" type="text" name="nameglow" maxlength="6">
-       <label for="namecolor">Name Color:</label>
-       <input class="jscolor" type="text" name="namecolor" maxlength="6">
-       <label for="bubbletext">Bubble Text Glow:</label>
-       <input class="jscolor" type="text" name="bubbletext" maxlength="6">
-       <label for="bubblecolor">Bubble Color:</label>
-       <input class="jscolor" type="text" name="bubblecolor" maxlength="6">
-       <label for="ringcolor">Ring Color:</label>
-       <input class="jscolor" type="text" name="ringcolor" maxlength="6">
-       <label for="chatglow">Chat Glow:</label>
-       <input class="jscolor" type="text" name="chatglow" maxlength="6">
-       <label for="penguinglow">Penguin Glow:</label>
-       <input class="jscolor" type="text" name="penguinglow" maxlength="6">
-       <label for="speed">Speed:</label>
-       <div class="slider">
-       <output id="rangevalue">10</output>
-       <input type = "range" min="0" max="100" name="speed" onchange="rangevalue.value=value"/>
-       </div>
-       <input type="submit" id="login-button" name="update" value="Update">
-       <span><?php echo $strError; ?></span>
-       <span><?php echo $strMessage; ?></span>
-</form>
-</center>
+<center>             
+<h1>Normal Users Commands: </h1>
+<table>
+  <tr>
+    <th><h2>Command</h2></th>
+    <th><h2>Example</h2></th> 
+    <th><h2>Info</h2></th>
+  </tr>
+  <tr>
+    <td>!SID</td>
+    <td>!SID</td> 
+    <td>Know your penguin ID</td>
+  </tr>
+  <tr>
+    <td>!SPING</td>
+    <td>!SPING</td> 
+    <td>Check if the server is alive</td>
+  </tr>
+  <tr>
+    <td>!AI</td>
+    <td>!AI 127</td> 
+    <td>Add an Item using an Item ID</td>
+  </tr>
+  <tr>
+    <td>!AAI</td>
+    <td>!AAI</td> 
+    <td>Add all items</td>
+  </tr>
+  <tr>
+    <td>!AF</td>
+    <td>!AF 64</td> 
+    <td>Add a Furniture using a Furniture ID</td>
+  </tr>
+  <tr>
+    <td>!AIG</td>
+    <td>!AIG 8</td> 
+    <td>Add an Igloo using a Igloo ID</td>
+  </tr>
+  <tr>
+    <td>!CIF</td>
+    <td>!CIF 12</td> 
+    <td>Change the Igloo Flooring using a Floor ID</td>
+  </tr>
+  <tr>
+    <td>!SAVEOUTFIT</td>
+    <td>!SAVEOUTFIT Outfit-name</td> 
+    <td>Save current penguin outfit</td>
+  </tr>
+  <tr>
+    <td>!GPOUTFIT</td>
+    <td>!GPOUTFIT</td> 
+    <td>Get previous penguin outfit</td>
+  </tr>
+  <tr>
+    <td>!GSOUTFIT</td>
+    <td>!GSOUTFIT Outfit-name</td> 
+    <td>Get saved penguin outfit by outfit name</td>
+  </tr>
+  <tr>
+    <td>!CPC</td>
+    <td>!CPC</td> 
+    <td>Remove all clothing items from the penguin</td>
+  </tr>
+  <tr>
+    <td>!CNICK</td>
+    <td>!CNICK Texas</td> 
+    <td>Change your nickname</td>
+  </tr>
+  <tr>
+    <td>!AC</td>
+    <td>!AC 5000</td> 
+    <td>Add a specific amount of coins</td>
+  </tr>
+  <tr>
+    <td>!GOTO</td>
+    <td>!GOTO Kevin</td> 
+    <td>Go to a particular user</td>
+  </tr>
+  <tr>
+    <td>!PCLONE</td>
+    <td>!PCLONE Kevin</td> 
+    <td>Clone a penguin by their name</td>
+  </tr>
+  <tr>
+    <td>!DEC</td>
+    <td>!DEC</td> 
+    <td>Disable/Enable cloning</td>
+  </tr>
+  <tr>
+    <td>!FIND</td>
+    <td>!FIND Trevor</td> 
+    <td>Find out where a user is in the server</td>
+  </tr>
+   <tr>
+    <td>!SCOUNT</td>
+    <td>!SCOUNT</td> 
+    <td>The Bot shows the number of users in the server</td>
+  </tr>
+   <tr>
+    <td>!RCOUNT</td>
+    <td>!RCOUNT</td> 
+    <td>The Bot shows the number of the users in the current room</td>
+  </tr>
+   <tr>
+    <td>!JR</td>
+    <td>!JR 100</td> 
+    <td>Join a room using a Room ID</td>
+  </tr>
+</table>
+
+<h1>VIP Commands: </h1>
+<table>
+<tr>
+    <th><h2>Command</h2></th>
+    <th><h2>Example</h2></th> 
+    <th><h2>Info</h2></th>
+  </tr>        
+  <td>!SPS</td>
+    <td>!SPS 50</td> 
+    <td>Set the penguin Speed ranging from 0 to 100</td>
+  </tr>
+   <tr>
+    <td>!SNG</td>
+    <td>!SNG 0xFFD700</td> 
+    <td>Set/Change your Name Glow</td>
+  </tr> 
+  <tr>
+    <td>!SNC</td>
+    <td>!SNC 0xFFD700</td> 
+    <td>Set/Change your Name Color</td>
+  </tr>
+   <tr>
+    <td>!SBC</td>
+    <td>!SBC 0xFFD700</td> 
+    <td>Set/Change your Bubble Color</td>
+  </tr>
+  <tr>
+    <td>!SBG</td>
+    <td>!SBG 0xFFD700</td> 
+    <td>Set/Change your Bubble Glow</td>
+  </tr>
+   <tr>
+    <td>!SBT</td>
+    <td>!SBT 0xFFD700</td> 
+    <td>Set/Change your BubbleText Color</td>
+  </tr>
+   <tr>
+    <td>!SRC</td>
+    <td>!SRC 0xFFD700</td> 
+    <td>Set/Change your Ring Color</td>
+  </tr>
+   <tr>
+    <td>!SCG</td>
+    <td>!SCG 0xFFD700</td> 
+    <td>Set/Change your Chat Text Glow</td>
+  </tr>
+  <tr>
+    <td>!SPG</td>
+    <td>!SPG 0xFFD700</td> 
+    <td>Set/Change your Penguin Glow</td>
+  </tr>
+  <tr>
+    <td>!SMG</td>
+    <td>!SMG 0xFFD700</td> 
+    <td>Set/Change your Mood Glow</td>
+  </tr>
+  <tr>
+    <td>!SMC</td>
+    <td>!SMC 0xFFD700</td> 
+    <td>Set/Change your Mood Color</td>
+  </tr>
+  <tr>
+    <td>!SSG</td>
+    <td>!SSG 0xFFD700</td> 
+    <td>Set/Change the Snowball Glow</td>
+  </tr>
+  <td>!PBLEND</td>
+    <td>!PBLEND Invert</td> 
+    <td>Change the Blend Mode of the penguin</td>
+  </tr>
+  <td>!PSS</td>
+    <td>!PSS 420 421</td> 
+    <td>Change the penguin size</td>
+  </tr>
+  <td>!PALPHA</td>
+    <td>!PALPHA 6</td> 
+    <td>Change the visibility of the penguin</td>
+  </tr>
+</table>
+
+<h1>Staff Commands: </h1>
+<table>
+<tr>
+    <th><h2>Command</h2></th>
+    <th><h2>Example</h2></th> 
+    <th><h2>Info</h2></th>
+  </tr>        
+  <td>#BAN</td>
+    <td>#BAN Trevor</td> 
+    <td>Ban a user by their name</td>
+  </tr>
+   <tr>
+    <td>#KBC</td>
+    <td>#KBC Trevor</td> 
+    <td>Kick and Ban a user by their name at the same time</td>
+  </tr> 
+  <tr>
+    <td>#TBAN</td>
+    <td>#TBAN Trevor</td> 
+    <td>Ban a user by their name for a specific amount of time(24h, 48h, 78h)</td>
+  </tr>
+   <tr>
+    <td>#KICK</td>
+    <td>#KICK Trevor</td> 
+    <td>Kick a user by their name from the server</td>
+  </tr>
+   <tr>
+    <td>#UNBAN</td>
+    <td>#UNBAN Trevor</td> 
+    <td>Unban a user by their name</td>
+  </tr>
+   <tr>
+    <td>#SHUTDOWN</td>
+    <td>#SHUTDOWN</td> 
+    <td>Shutdown the server</td>
+  </tr>
+   <tr>
+    <td>#GLOBAL</td>
+    <td>#GLOBAL this is a test message</td> 
+    <td>Send a message across the server</td>
+  </tr>
+  <tr>
+    <td>#SUMMON</td>
+    <td>#SUMMON Trevor</td> 
+    <td>Summon a user by their name to the room you're in</td>
+  </tr>
+  <tr>
+    <td>#MIRROR</td>
+    <td>#MIRROR Trevor</td> 
+    <td>Activate mirror on a user by their name</td>
+  </tr>
+  <tr>
+    <td>#UNMIRROR</td>
+    <td>#UNMIRROR Trevor</td> 
+    <td>Deactivate mirror on a user by their name</td>
+  </tr>
+</table>
+</center>			  
 
 </div>
 <div class="footer">&copy; 2016-2017 Luna &#8482; All Rights Reserved</div>
 </div>
 </body>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
-<script src="../js/jscolor.js"></script>
-<script src="../js/index.js"></script>
+<script src="js/index.js"></script>
 </html>
