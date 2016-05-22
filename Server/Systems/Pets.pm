c3e312ce222e76da9257b9283f2909dbe3657f95
Neil <leneilmario@gmail.com>
2016-05-22 16:08:07 +0000

    Update Pets.pm

--- Server/Systems/Pets.pm
+++ Server/Systems/Pets.pm
@@ -15,7 +15,6 @@ method handleAdoptPuffle($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $puffleID = $arrData[5];
        my $puffleName = $arrData[6];
-       return if ($puffleName !~ /^[a-zA-Z0-9]+$/);
        if ($objClient->{coins} < 800) {
            return $objClient->sendError(401);
        }
@@ -82,7 +81,7 @@ method handlePufflePip($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $puffleID = $arrData[5];
        return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]));
-       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       my $petDetails = $self->{modules}->{mysql}->getPuffleByOwner($puffleID, $objClient->{ID});
        $objClient->sendRoom('%xt%pir%-1%' . $petDetails->{puffleID} . '%' . $arrData[6] . '%' . $arrData[7] . '%');
 }
 
@@ -117,7 +116,7 @@ method handlePufflePir($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $puffleID = $arrData[5];
        return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]));
-       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       my $petDetails = $self->{modules}->{mysql}->getPuffleByOwner($puffleID, $objClient->{ID});
        $objClient->sendRoom('%xt%pir%-1%' . $petDetails->{puffleID} . '%' . $arrData[6] . '%' . $arrData[7] . '%');
 }
 
@@ -164,7 +163,7 @@ method handlePuffleWalk($strData, $objClient) {
        my $puffleID = $arrData[5];
        my $blnWalk = $arrData[6];
        return if (!int($puffleID) || !int($blnWalk));
-       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       my $petDetails = $self->{modules}->{mysql}->getPuffleByOwner($puffleID, $objClient->{ID});
        if ($petDetails) {
            my $walkStr = $petDetails->{puffleID} . '|' . $petDetails->{puffleName} . '|' . $petDetails->{puffleType} . '|' . $petDetails->{puffleHealth} . '|' . $petDetails->{puffleEnergy} . '|' . $petDetails->{puffleRest} . '|0|0|0|0|0|0'; # Dont know what the rest are
            if ($blnWalk eq 1) {
