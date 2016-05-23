90ea564d05584dc44d044a6f36ca91d315e06b86
Neil <leneilmario@gmail.com>
2016-05-23 09:52:33 +0000

    Update Stamps.pm

--- Server/Systems/Stamps.pm
+++ Server/Systems/Stamps.pm
@@ -21,13 +21,13 @@ method handleGetPlayersStamps($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $intPID = $arrData[5];
        return if (!int($intPID));
-       my $strStamps = $self->{modules}->{mysql}->getStampsByID($intPID);
+       my $strStamps = $self->{child}->{modules}->{mysql}->getStampsByID($intPID);
        $objClient->sendXT(['gps', '-1', $intPID, $strStamps]);
 }
 
 method handleGetMyRecentlyEarnedStamps($strData, $objClient) {
        my $intID = $objClient->{ID};
-       my $strREStamps = $self->{modules}->{mysql}->getRestampsByID($intID);
+       my $strREStamps = $self->{child}->{modules}->{mysql}->getRestampsByID($intID);
        $objClient->sendXT(['gmres', '-1', $intID, $strREStamps]);
        $self->{child}->{modules}->{mysql}->updateTable('users', 'restamps', '', 'ID', $intID);
 }
@@ -36,7 +36,7 @@ method handleGetStampBookCoverDetails($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $intPID = $arrData[5];
        return if (!int($intPID));
-       my $strCover = $self->{modules}->{mysql}->getStampbookCoverByID($intPID);
+       my $strCover = $self->{child}->{modules}->{mysql}->getStampbookCoverByID($intPID);
        $objClient->write('%xt%gsbcd%-1%' . ($strCover ? $strCover : '1%1%1%1%'));     
 }
 
