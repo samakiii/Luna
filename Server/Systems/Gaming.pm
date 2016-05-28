5c9c8df74ec3545904bb6c7a20fc26d9f800f3e8
Neil <leneilmario@gmail.com>
2016-05-28 14:09:07 +0000

    Update Gaming.pm

--- Server/Systems/Gaming.pm
+++ Server/Systems/Gaming.pm
@@ -17,17 +17,17 @@ method new($resChild) {
 method handleGameOver($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $intCoins = $arrData[5];
-       if ($objClient->{room} <= 899 || $objClient->{room} >= 1000) {
-           return $objClient->sendXT(['zo', '-1', $objClient->{coins}, '', 0, 0, 0]);
-       }
+      # if ($objClient->{room} <= 899 || $objClient->{room} >= 1000) {
+      #     return $objClient->sendXT(['zo', '-1', $objClient->{coins}, '', 0, 0, 0]);
+       #}
        if ($intCoins > 99999) {
            $objClient->sendError(611);
            return $self->{child}->{modules}->{base}->removeClient($objClient->{sock});
        }
-       my $coins = round($intCoins / 10);
-       $objClient->setCoins($objClient->{coins} + $coins);
+       my $intTotalCoins = round(($objClient->{coins} + ($intCoins / 10))); 
+       $objClient->updateCoins($intTotalCoins);
        # No stamps yet, later?
-       $objClient->sendXT(['zo', '-1', $objClient->{coins}, '', 0, 0, 0]);
+      # $objClient->sendXT(['zo', '-1', $objClient->{coins}, '', 0, 0, 0]);
 }
 
 method handleMovePuck($strData, $objClient) {
