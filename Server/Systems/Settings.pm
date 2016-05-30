ead02877d364005870fdab52936100a447c76dea
Neil <leneilmario@gmail.com>
2016-05-30 08:43:13 +0000

    Update Settings.pm

--- Server/Systems/Settings.pm
+++ Server/Systems/Settings.pm
@@ -18,7 +18,15 @@ method handleUpdatePlayerClothing($strData, $objClient) {
        my $intItem = $arrData[5];
        my $strType = $arrCmd[1];
        my %arrTypes = (upc => 'colour', uph => 'head', upf => 'face', upn => 'neck', upb => 'body', upa => 'hand', upe => 'feet', upp => 'photo', upl => 'flag');
-       return if (!exists($arrTypes{$strType}));        
+       return if (!exists($arrTypes{$strType}));   
+       if ($strType eq 'upa' && $intItem == 0) {
+			my $strWalkingPuffle = $objClient->getWalkingPuffle;
+			if ($strWalkingPuffle ne '') {
+				my @arrInfo = split('|', $strWalkingPuffle);
+				my $intPuffle = $arrInfo[0];
+				$self->{child}->{modules}->{mysql}->updateWalkingPuffle(0, $intPuffle, $objClient->{ID});
+			}
+	   }
        $objClient->updatePlayerCard($strType, $arrTypes{$strType}, $intItem);
 }
 
