232d8c49390334fd19c84bf45405db998c0e94b0
Lynx <titshacking@users.noreply.github.com>
2016-06-13 22:58:00 +0530

    Create Survey.pm

---
+++ Server/Systems/Survey.pm
@@ -0,0 +1,31 @@
+package Survey;
+
+use strict;
+use warnings;
+
+use Method::Signatures;
+use feature qw(say);
+
+method handleSignIglooContest($strData, $objClient) {
+		my $strInfo = $objClient->{username} . ':' . $objClient->{ID};
+		$self->appendToFile('IglooContests.txt', $strInfo);
+}
+
+method handleDonateCoins($strData, $objClient) {
+		my @arrData = split('%', $strData);
+		my $intDonation = $arrData[6];
+		return if (!int($intDonation));
+		my $intRemaining = $objClient->{coins} - $intDonation;
+		my $strInfo = $objClient->{username} . ':' . $objClient->{ID} . '| Donated: ' . $intDonation;
+		$self->appendToFile('Donations.txt', $strInfo);
+		$objClient->setCoins($intRemaining);
+		$objClient->loadDetails;
+}
+
+method appendToFile($strFile, $strData) {
+		open(my $fh, '>>', $strFile);
+		say $fh $strData;
+		close $fh;
+}
+
+1;
