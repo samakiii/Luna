ff7f8735fbd2df1ac459d564dc7428fce9ca8861
Lynx <titshacking@users.noreply.github.com>
2016-06-15 21:45:44 +0530

    Igloo Contests and Donations now use the Database
    Igloo Contests and Donations now use the Database and you can only sign up once every 24 hours and donate every 1 hour

--- Server/Systems/Survey.pm
+++ Server/Systems/Survey.pm
@@ -4,34 +4,51 @@ use strict;
 use warnings;
 
 use Method::Signatures;
-use feature qw(say);
+use HTTP::Date qw(str2time);
 
 method new($resChild) {
-		my $obj = bless {}, $self;
-		$obj->{child} = $resChild;
-		return $obj;
+	my $obj = bless {}, $self;
+	$obj->{child} = $resChild;
+	return $obj;
 }
 
 method handleSignIglooContest($strData, $objClient) {
-		my $strInfo = $objClient->{username} . ':' . $objClient->{ID};
-		$self->appendToFile('IglooContests.txt', $strInfo);
+		my $intID = $objClient->{ID};
+		my $strUsername = $objClient->{username};
+		my $arrSignUps = $self->{child}->{modules}->{mysql}->checkJoinedIglooContest($intID);
+		foreach (values @{$arrSignUps}) {
+			my $intLastSignedTime = str2time($_->{signup_time});
+			my $intCurrTimestamp = time;
+			my $intTimeDiff = $intLastSignedTime - $intCurrTimestamp;
+			if ($intTimeDiff < 86400) { # check if last signed up time is less than 24 hours
+				return $objClient->sendError(213);
+			}
+		}
+		$self->{child}->{modules}->{mysql}->insertData('igloo_contest', ['ID', 'username'], [$intID, $strUsername]);
 }
 
 method handleDonateCoins($strData, $objClient) {
 		my @arrData = split('%', $strData);
 		my $intDonation = $arrData[6];
 		return if (!int($intDonation));
+		my $intID = $objClient->{ID};
+		my $strUsername = $objClient->{username};
+		my $arrDonations = $self->{child}->{modules}->{mysql}->getLastDonations($intID);
+		foreach (values @{$arrDonations}) {
+			my $intLastDonatedTime = str2time($_->{donate_time});
+			my $intCurrTimestamp = time;
+			my $intTimeDiff = $intLastDonatedTime - $intCurrTimestamp;
+			if ($intTimeDiff < 3600) { # check if last donated time is less than an hour
+				return $objClient->sendError(213);
+			}
+		}
+		if ($objClient->{coins} <= 0) { # check if client has no coins
+			$objClient->sendError(401);
+		}
 		my $intRemaining = $objClient->{coins} - $intDonation;
-		my $strInfo = $objClient->{username} . ':' . $objClient->{ID} . '| Donated: ' . $intDonation;
-		$self->appendToFile('Donations.txt', $strInfo);
+		$self->{child}->{modules}->{mysql}->insertData('donations', ['ID', 'username', 'donation'], [$intID, $strUsername, $intDonation]);
 		$objClient->setCoins($intRemaining);
 		$objClient->loadDetails;
-}
-
-method appendToFile($strFile, $strData) {
-		open(my $fh, '>>', $strFile);
-		say $fh $strData;
-		close $fh;
 }
 
 1;
